783b9e931c66909503dee57e593382a2fa7326d2
karolyulieth <135679636+karolyulieth@users.noreply.github.com>
2023-06-05 21:45:05 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+#include "stdlib.h"
+
+using namespace std;
+
+int main() 
+{ int suma=0;
+   for(int n=0;n<=10;n++)
+     { 
+       suma= suma+n;
+      }
+    cout<< "La suma de los numeros de 1 a 10 es" <<suma<<endl;
+}
